be1e9617a5cc08b9bb12677302af8f4b27e7ba4e
Piotr Wróbel <wrobelpiotr24@gmail.com>
2025-12-10 16:02:42 +0100

    feat(8.1): Implement Licznik with state persistence via localStorage

---
+++ Lab-05-React/task-08-blog/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import LicznikLocalStorage from './components/LicznikLocalStorage.tsx';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='header'>
+          <span>⭐</span>
+          <h1> Rozwiązania zadań z Reacta <br/> z wykorzystaniem Vite i TypeScript</h1>
+          <span>⭐</span>
+      </div>
+      <h2>Zadanie 8.1: Licznik z Pamięcią (LocalStorage)</h2>
+      <LicznikLocalStorage />
+      
+      {/* Tu będzie router dla Zadania 8.2 */}
+      <h2>Zadanie 8.2: Aplikacja Blog z React Router</h2>
+      {/* <BlogRouter /> */}
+      
+    </div>
+  );
+}
+
+export default App;
