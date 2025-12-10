330c1549d3954cd4ece75e7ed17ce90d39b69599
Piotr Wróbel <wrobelpiotr24@gmail.com>
2025-12-10 12:29:18 +0100

    feat: Initialize Vite + React (TS) project setup

---
+++ Lab-05-React/vite-project/src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='header'>
+        <span>⭐</span>
+        <h1> Rozwiązania zadań z Reacta <br/> z wykorzystaniem Vite i TypeScript</h1>
+        <span>⭐</span>
+      </div>
+
+      {/* ============================================================
+        ZADANIE 1: KOSZYK
+        ============================================================
+      */}
+      <h2>Zadanie 1: Koszyk (1.1, 1.2)</h2>
+
+      {/* ============================================================
+        ZADANIE 2: LICZNIKI
+        ============================================================
+      */}
+      <h2>Zadanie 2: Liczniki (2.1, 2.2)</h2>
+
+
+    </div>
+  );
+}
+
+export default App;
